1d191af189519ae55de5a7cfd9f537f5ee178c17
Devan <devannathvani@hotmail.com>
2018-03-04 00:44:57 +0000

    Initial commit

---
+++ app/src/main/java/com/example/devan/tfldriver_advanced/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.devan.tfldriver_advanced;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void openbakerloo(View view) {
+        Intent openbakerloointent = new Intent(MainActivity.this, linesactivity.class);
+        openbakerloointent.putExtra("Uline", "Bakerloo");
+        openbakerloointent.putExtra ("Ustation",R.array.bakerloostations);
+        startActivity(openbakerloointent);
+        //array
+
+    }
+    public void opencentral(View view) {
+        Intent opencentralintent = new Intent(MainActivity.this, linesactivity.class);
+        opencentralintent.putExtra("Uline", "Central");
+        opencentralintent.putExtra ("Ustation",R.array.centralstations);
+        startActivity(opencentralintent);
+    }
+    public void opencircle(View view) {
+        Intent opencirclelintent = new Intent(MainActivity.this, linesactivity.class);
+        opencirclelintent.putExtra("Uline", "Circle");
+        opencirclelintent.putExtra ("Ustation",R.array.circlestations);
+        startActivity(opencirclelintent);
+    }
+    public void opendistrict(View view) {
+        Intent opendistrictlintent = new Intent(MainActivity.this, linesactivity.class);
+        opendistrictlintent.putExtra("Uline", "District");
+        opendistrictlintent.putExtra ("Ustation",R.array.distictstations);
+        startActivity(opendistrictlintent);
+    }
+    public void openhammersmith_and_city(View view) {
+        Intent openhammersmith_and_city = new Intent(MainActivity.this, linesactivity.class);
+        openhammersmith_and_city.putExtra("Uline","Hammersmith-City");
+        openhammersmith_and_city.putExtra ("Ustation",R.array.Hammersmith_and_Citystations);
+        startActivity(openhammersmith_and_city);
+    }
+    public void openjubilee(View view) {
+        Intent openjubilee = new Intent(MainActivity.this, linesactivity.class);
+        openjubilee.putExtra("Uline", "Jubilee");
+        openjubilee.putExtra ("Ustation",R.array.Jubileestations);
+        startActivity(openjubilee);
+    }
+    public void openmetropolitan(View view) {
+        Intent openmetropolitan = new Intent(MainActivity.this, linesactivity.class);
+        openmetropolitan.putExtra("Uline", "Metropolitan");
+        openmetropolitan.putExtra ("Ustation",R.array.metropolitanstations);
+
+        startActivity(openmetropolitan);
+    }
+    public void openpiccadilly(View view) {
+        Intent openpiccadilly = new Intent(MainActivity.this, linesactivity.class);
+        openpiccadilly.putExtra("Uline", "Piccadilly");
+        openpiccadilly.putExtra ("Ustation",R.array.picadillystations);
+        startActivity(openpiccadilly);
+    }
+    public void openvictoria(View view) {
+        Intent openvictoria = new Intent(MainActivity.this, linesactivity.class);
+        openvictoria.putExtra("Uline", "Victoria");
+        openvictoria.putExtra ("Ustation",R.array.victoriastations);
+        startActivity(openvictoria);
+    }
+  /*  public void openwaterloo_and_city(View view) {
+        Intent openwaterloo_and_city = new Intent(MainActivity.this, linesactivity.class);
+        openwaterloo_and_city.putExtra("Uline", "Waterloo_and_City");
+
+        startActivity(openwaterloo_and_city);
+    }waterloo-city*/
+
+    public void opennorthern(View view) {
+        Intent opennorthern = new Intent(MainActivity.this, linesactivity.class);
+        opennorthern.putExtra("Uline", "Northern");
+        opennorthern.putExtra ("Ustation",R.array.northernstations);
+        startActivity(opennorthern);
+    }
+}
---
+++ app/src/main/java/com/example/devan/tfldriver_advanced/linesactivity.java
@@ -0,0 +1,136 @@
+package com.example.devan.tfldriver_advanced;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+
+public class linesactivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private RequestQueue mQueue;
+    //Global declaration
+    String linename,result;
+    Spinner spinner;
+    TextView spinnerview;
+
+    @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_lines);
+            TextView mTextview = findViewById(R.id.lines);
+
+            //Uline will change to each specific line
+            Intent intent = getIntent();
+            linename = intent.getStringExtra("Uline");
+
+            //Produces title
+            String Title = linename;
+            Title = Title.replaceAll("_", " ");
+            mTextview.setText(Title + " Line");
+
+
+            //call on different arrays
+            Intent b = getIntent();
+            int a = b.getIntExtra("Ustation",-1);
+
+            //spinner
+            spinner = findViewById(R.id.spinner);
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, a, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setOnItemSelectedListener(this);
+            spinner.setAdapter(adapter);
+
+
+            mQueue = Volley.newRequestQueue(this);
+            //the parse is the update button
+            Button button_parse = findViewById(R.id.button_parse);
+            //when the button is clicked run the following method
+            button_parse.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    jsonParse();
+            }
+        });
+    }
+
+    private void jsonParse() {
+        String url = "https://api.tfl.gov.uk/line/" + linename + "/arrivals";
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                //the user enters the train number
+                EditText train_number;
+                train_number = findViewById(R.id.train_number);
+                String str = train_number.getText().toString();
+                String sta = spinnerview.getText().toString();
+                String vehicleId;
+                try {
+                    TextView mTextViewResult;
+                    mTextViewResult = findViewById(R.id.text_view_result);
+                    for (int i = 0; i < response.length(); i++) {
+                        JSONObject x = response.getJSONObject(i);
+
+                        vehicleId = x.getString("vehicleId");
+                        if (vehicleId.equals(str)){
+                          mTextViewResult.append(vehicleId);
+
+                        }
+
+                        Integer timeToStation = x.getInt("timeToStation");
+                        String towards = x.getString("towards");
+                        String currentLocation = x.getString("currentLocation");
+
+                        mTextViewResult.append("Train Number " + str + "\n" + "Currently " + currentLocation + "\n" + "Arriving " + towards + "\n" + "in " + timeToStation/60+ " Minutes " + "\n\n\n");
+
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
+    }
+    @Override
+    public void onItemSelected(final AdapterView<?> parent,View view, int i, long l) {
+        //String stations = parent.getItemAtPositon(position).toString();
+        //toast.makeText(parent.getContext(),stations, Toast.LENGTH_SHORT).show();
+        result = spinner.getSelectedItem().toString();
+        spinnerview = (TextView)findViewById(R.id.spinnerview);
+        spinnerview.setText(result);
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+    }
+}
+
+
+
+
